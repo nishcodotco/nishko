91799337a76e85a12f7f9f05d11b2f7947815180
nishko <commit@nishko.com>
2024-12-16 21:18:58 -0500

    adding routes

---
+++ docs/chunk-SMWJKLLA.js
@@ -0,0 +1,1 @@
+import{ka as t,qa as n,ra as s,ta as l}from"./chunk-XZSYBVLY.js";var p=class o{constructor(){console.log("BLOGS COMPONENT => ")}static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["page-blogs"]],decls:2,vars:0,template:function(e,c){e&1&&(n(0,"p"),l(1,"Welcome to my blogs page"),s())},encapsulation:2})};export{p as BlogsComponent};
