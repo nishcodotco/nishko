e4dcdac954a9c74dab5208918d5137935096388d
nishko <commit@nishko.com>
2024-12-16 20:37:37 -0500

    upated routing of the application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withHashLocation } from '@angular/router';
+import { APP_BASE_HREF, PathLocationStrategy } from '@angular/common';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+
+    // ROUTING STRATEGY
+    provideRouter(routes),
+    // provideRouter(routes, withHashLocation()),  // For # based routing
+    // { provide: APP_BASE_HREF, useValue: '/my/app/' }, // For custom base path
+  ]
 };
---
+++ src/app/pages/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-projects',
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
